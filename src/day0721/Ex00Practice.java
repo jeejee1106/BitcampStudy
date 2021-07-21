2850cfc77f17e0b9255c37c066ee52f11164183b
PC <PC@DESKTOP-EGKU3DO>
2021-07-21 23:01:07 +0900

    commit practice!!

---
+++ src/day0721/Ex00Practice.java
@@ -0,0 +1,11 @@
+package day0721;
+
+public class Ex00Practice {
+
+	public static void main(String[] args) {
+		System.out.println("this is a toy class file!!");
+		System.out.println("github practice");
+		System.out.println("please...please...please.......");
+	}
+
+}
---
+++ src/day0722/Ex_RealToyFile.java
@@ -0,0 +1,10 @@
+package day0722;
+
+public class Ex_RealToyFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("commit please..........");
+	}
+
+}
