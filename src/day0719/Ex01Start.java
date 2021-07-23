134ab06902a75879c83d689f20467fedb381bb30
bitcamp <bitcamp@DESKTOP-HV2UM4G>
2021-07-23 09:04:27 +0900

    add 0719 add 0719 study file

---
+++ src/day0719/Ex01Start.java
@@ -0,0 +1,11 @@
+package day0719;
+
+public class Ex01Start {
+
+	public static void main(String[] args) {
+		System.out.println("이클립스로 자바 실행하기");
+		System.out.println("Hello Java!!!");
+		System.out.println("내 나이는 " + args[0] + "세 입니다.");
+	}
+
+}
---
+++ src/day0719/Ex02MyHobby.java
@@ -0,0 +1,16 @@
+package day0719;
+
+public class Ex02MyHobby {
+
+	public static void main(String[] args) {
+		
+		System.out.println("내 취미는 " + args[0] + "입니다.");
+		System.out.println("내 이름은 " + args[1] + " 입니다.");
+		System.out.println("내 나이는 " + args[2] + " 입니다.");
+		//<args배열에 index주는 방법>
+		//한번이라도 실행을 한 후 Run버튼 옆에 화살표 누르고
+		//Run Configuration - Arguments - Program arguments에
+		//인자값 세개를 띄어쓰기로 구분하며 넣어준다.
+	}
+
+}
